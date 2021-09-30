a702ac36a8e7bdde2c11ea20b890918ff7984df2
willy24692485 <willy24692485@gmail.com>
2021-09-30 20:16:17 +0800

    first

---
+++ main.py
@@ -0,0 +1,6 @@
+import geometry.point
+result=geometry.point.ditance(3,4)
+print("距離",result)
+import geometry.line as line  # 別名
+result=line.slope(1,1,3,3)
+print("斜率",result)
---
+++ test-main.py
@@ -0,0 +1,6 @@
+import test.point
+result=test.point.distance(7,24)
+print(result)
+import test.average
+result=test.average.avg(7,11,23,45,77)
+print(result)
